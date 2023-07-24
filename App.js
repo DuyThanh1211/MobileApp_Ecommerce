409d4f8ffe3bb7f406b73038e991578770977683
HvThang02 <88171110+HvThang02@users.noreply.github.com>
2023-07-24 23:31:42 +0700

    ProductDetails

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import ProductItem from "./src/components/ProductItem";
+import ProductPage from "./src/components/ProductPage";
 import { createStackNavigator } from "@react-navigation/stack";
 import ProductDetails from "./src/components/ProductDetails";
 import { NavigationContainer } from "@react-navigation/native";
+import HomePage from "./src/components/HomePage";
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     // <>
-    //   <ProductItem />
+    //   <ProductDetails />
     // </>
+
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="List Product" component={ProductItem} />
+        <Stack.Screen name="List Product" component={ProductPage} />
         <Stack.Screen name="Product Details" component={ProductDetails} />
       </Stack.Navigator>
     </NavigationContainer>
