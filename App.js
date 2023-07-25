c24a234564509cbe9601a5168eb724cff2b100fc
AnhPhatt <86426105+AnhPhatt@users.noreply.github.com>
2023-07-25 22:57:22 +0700

    Update_Search

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-// import ProductItem from "./src/components/ProductItem";
 import Login from "./src/components/Login";
 import Register from "./src/components/Register";
 import ProductPage from "./src/components/ProductPage";
 import { createStackNavigator } from "@react-navigation/stack";
 import ProductDetails from "./src/components/ProductDetails";
 import { NavigationContainer } from "@react-navigation/native";
-// import HomePage from "./src/components/HomePage";
+import HomePage from "./src/components/HomePage";
+
 export default function App() {
   const Stack = createStackNavigator();
   return (
